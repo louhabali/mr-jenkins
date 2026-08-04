2894c9b3ebc7c2e4ea8a4bba5af2ea86104a83f9
iichi <you@example.com>
2026-08-04 13:17:48 +0100

    test 6 : trigger the pipeline 7

--- product_service/src/test/java/buy01/product_service/ProductServiceTest.java
+++ product_service/src/test/java/buy01/product_service/ProductServiceTest.java
@@ -94,7 +94,7 @@ class ProductServiceTest {
                                 .price(1999.99)
                                 .quantity(5)
                                 .userId("seller-1")
-                                .imageUrls(null)
+                                .imageUrls(List.of("image.png"))
                                 .build();
 
                 when(mediaClient.uploadImages(any(MultipartFile[].class)))
